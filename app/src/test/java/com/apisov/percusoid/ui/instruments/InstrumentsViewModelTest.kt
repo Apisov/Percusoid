5bfc9d98e5d1ac18887e659cea9b0c043ebb6b25
Apisov <apisovp@gmail.com>
2018-08-08 19:55:32 -0400

    Add tests in InstrumentsViewModelTest

--- app/src/test/java/com/apisov/percusoid/ui/instruments/InstrumentsViewModelTest.kt
+++ app/src/test/java/com/apisov/percusoid/ui/instruments/InstrumentsViewModelTest.kt
@@ -4,7 +4,9 @@ import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.apisov.percusoid.data.Instrument
 import com.apisov.percusoid.data.source.InstrumentsRepository
 import com.apisov.percusoid.util.rx.ImmediateSchedulerProvider
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
@@ -31,11 +33,27 @@ class InstrumentsViewModelTest {
     }
 
     @Test
-    fun loadInstruments() {
+    fun loadNonEmptyInstruments() {
         `when`(repository.getInstruments()).thenReturn(Flowable.just(instruments))
         viewModel.start()
 
         verify(repository, times(1)).getInstruments()
-        assertEquals(viewModel.instruments, instruments)
+        assertEquals(instruments, viewModel.instruments)
+    }
+
+    @Test
+    fun loadInstrumentsAndUpdate() {
+        var emitter: FlowableEmitter<List<Instrument>>? = null
+        `when`(repository.getInstruments())
+            .thenReturn(Flowable.create({
+                emitter = it
+                it.onNext(instruments)
+            }, BackpressureStrategy.DROP))
+
+        viewModel.start()
+
+        val newInstruments = listOf(Instrument())
+        emitter?.onNext(newInstruments)
+        assertEquals(newInstruments, viewModel.instruments)
     }
 }
